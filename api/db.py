3be08fc7498bb0b49c260a4857d871fbcd83c769
Yuichiro Iijima <jimaichiro@gmail.com>
2021-12-14 02:04:16 +0900

    feat: DB接続環境を構築

---
+++ api/db.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import create_aync_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8"
+
+async_engine = create_aync_engine(ASYNC_DB_URL, echo=True)
+async_session = sessionmaker(
+  autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
+)
+
+Base = declarative_base()
+
+async def geb_db():
+  async with async_session() as session:
+    yield session
