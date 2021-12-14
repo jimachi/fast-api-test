376d092da5a4eb8fc61736a9f9e68487943b6e5c
Yuichiro Iijima <jimaichiro@gmail.com>
2021-12-15 01:28:56 +0900

    feat: POSTの実装

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,5 +1,9 @@
 from typing import List
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+import api.cruds.task as task_crud
+from api.db import get_db
 
 import api.schemas.task as task_schema
 
@@ -10,8 +14,9 @@ async def list_tasks():
   return [task_schema.Task(id=1, title="１つ目のTODOタスク")]
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate):
-  return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+async def create_task(task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
+  return await task_crud.create_task(db, task_body)
+  # return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
 async def update_task(task_id: int, task_body: task_schema.TaskCreate):
