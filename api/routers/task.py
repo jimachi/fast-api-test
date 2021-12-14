db2ca45b0a201e427d4634fd97d24f1eac5990df
Yuichiro Iijima <jimaichiro@gmail.com>
2021-12-15 01:35:40 +0900

    feat: GET処理の追加

--- api/routers/task.py
+++ api/routers/task.py
@@ -10,13 +10,12 @@ import api.schemas.task as task_schema
 router = APIRouter()
 
 @router.get("/tasks", response_model=List[task_schema.Task])
-async def list_tasks():
-  return [task_schema.Task(id=1, title="１つ目のTODOタスク")]
+async def list_tasks(db: AsyncSession = Depends(get_db)):
+  return await task_crud.get_tasks_with_done(db)
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
 async def create_task(task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
   return await task_crud.create_task(db, task_body)
-  # return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
 async def update_task(task_id: int, task_body: task_schema.TaskCreate):
